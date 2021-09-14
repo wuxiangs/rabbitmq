bf5b065275af2f4d3b215b991436777a491b5390
wuxiang <1325668250@qq.com>
2021-09-14 15:59:17 +0800

    提交

---
+++ consumer-springboot/src/test/java/com/aisino/consumerspringboot/ConsumerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisino.consumerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
